ed07f966927d4648fd17d14d00616660bd862b17
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-06-12 20:29:59 +0200

    init

---
+++ src/main/java/com/smarthome/shuserservice/ShUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smarthome.shuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShUserServiceApplication.class, args);
+    }
+
+}
