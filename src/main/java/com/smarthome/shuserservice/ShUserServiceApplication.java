be4eb920be186bec0ea110d6a3680972c845ac18
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-07-03 21:43:01 +0200

    enable eureka client

--- src/main/java/com/smarthome/shuserservice/ShUserServiceApplication.java
+++ src/main/java/com/smarthome/shuserservice/ShUserServiceApplication.java
@@ -3,16 +3,17 @@ package com.smarthome.shuserservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaRepositories("com.smarthome.shuserservice.repo")
 @EntityScan(basePackageClasses = {
         ShUserServiceApplication.class,
         Jsr310JpaConverters.class
 })
-//@EnableEurekaClient
 public class ShUserServiceApplication {
 
     public static void main(String[] args) {
